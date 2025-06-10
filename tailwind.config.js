287f1cddba5cdaa511a8296e599e6cea0ce6f31d
danyaffa <leffleryd@gmail.com>
2025-06-11 09:11:53 +1000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./public/pages/**/*.{html,js}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        adlblue: '#3b82f6',
-        adldark: '#1e293b',
-      }
-    }
-  },
-  plugins: []
+  content: ['./public/index.html', './public/pages/**/*.{html,js}'],
+  theme: { extend: {} },
+  plugins: [],
 }
