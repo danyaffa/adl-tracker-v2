989228bdf330f56881d58b31133fcde91f3a529a
danyaffa <leffleryd@gmail.com>
2025-06-10 19:50:13 +1000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./pages/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        adlblue: '#3b82f6',
+        adldark: '#1e293b',
+      }
+    }
+  },
+  plugins: [],
+}
